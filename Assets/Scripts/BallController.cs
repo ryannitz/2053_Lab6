846bfde7bf5b47d3b432205dc19ad0aa249f8278
ryannitz <ryan.nitz01@gmail.com>
2020-02-19 16:57:45 -0400

    Color and initial ball controller added

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    Vector3 initForce;
+
+    public AudioSource myAudio;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+
+        Vector3 forceDirection = new Vector3(Random.Range(-1f, -.5f), 0, Random.Range(-1f, -.5f));
+        forceDirection.Normalize();
+
+        float forcePower = 15;
+        initForce = forcePower * forceDirection;
+
+        //initial push onto the ball in the forceDirection with forcePower
+        rb.AddForce(initForce, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (myAudio != null && !myAudio.isPlaying &&
+        myAudio.clip != null && myAudio.clip.loadState == AudioDataLoadState.Loaded)
+        {
+            myAudio.Play();
+        }
+    }
+}
